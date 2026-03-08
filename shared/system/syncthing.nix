7de65afc36862273909c94b1dab10e20ea1c884c
Larry Hu <199412713+shen-solex@users.noreply.github.com>
2026-03-08 11:52:43 -0400

    Syncthing config

--- shared/system/syncthing.nix
+++ shared/system/syncthing.nix
@@ -8,6 +8,15 @@
       gui = {
         user = "lhu";
       };
+      devices = {
+        "iphone" = { id = "6XA25GM-VL2SKAY-JNC5LVH-5BT3BOV-HPGFWLQ-OTPXF3G-XYMSX2N-EHO43AQ"; };
+      };
+      folders = {
+        ".keepassxc" = {
+          path = "/home/lhu/.keepassxc";
+          devices = [ "iphone" ];
+        };
+      };
     };
     guiPasswordFile = config.sops.secrets.syncthing_passwd.path;
   };
