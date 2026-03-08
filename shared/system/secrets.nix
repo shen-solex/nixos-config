ec67dfa7cb2234125ab974abe03458a8cb762098
Larry Hu <199412713+shen-solex@users.noreply.github.com>
2026-03-07 23:56:55 -0500

    Add secrets

--- shared/system/secrets.nix
+++ shared/system/secrets.nix
@@ -1,5 +1,23 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, inputs, ... }:
+let
+  secretspath = builtins.toString inputs.secrets;
+in
 {
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
   
+  sops = {
+    defaultSopsFile = "${secretspath}/secrets.yaml";
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
+    secrets = {
+      lhu_passwd = {
+        neededForUsers = true;
+      };
+    };
+  };
 }
